00f891008bf814d3ceac04088cb886f494caafc8
Anthony Fisher <afisher.web@gmail.com>
2024-01-31 17:17:04 +0000

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,19 +4,15 @@ export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'yellow': '#FFE81F',
+      'black': '#000000',
+      'silver' : '#B3B6B7',
+      'violet': '#54457F',
+      'green': '#4C6663'
+
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: ['inter', 'sans-serif'],
     },
   }
 } satisfies Config
